e103c6c88ea496de9aaacba01d88448a7b530a7a
oc-garcia <octaviogarcia1988@gmail.com>
2024-07-25 00:01:33 -0300

    chore: Refactor database connection configuration
    The commit refactors the database connection configuration in the `db.ts` file. It updates the code to use a configuration object instead of a connection string. This change improves readability and maintainability of the code.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,47 +1,34 @@
 import { env } from '@/env'
 import { Pool, PoolClient } from 'pg'
 
-const connectionString =
-  process.env.DB_URL ||
-  `postgresql://${env.DATABASE_USER}:${env.DATABASE_PASSWORD}@postgres:${env.DATABASE_PORT}/${env.DATABASE_NAME}`
+const CONFIG = {
+  user: env.DATABASE_USER,
+  host: env.DATABASE_HOST,
+  database: env.DATABASE_NAME,
+  password: env.DATABASE_PASSWORD,
+  port: env.DATABASE_PORT,
+}
 
 class Database {
   private pool: Pool
   private client: PoolClient | undefined
 
   constructor() {
-    this.pool = new Pool({
-      connectionString,
-    })
-
-    this.pool.on('error', (err) => {
-      console.error('Unexpected error on idle client', err)
-      process.exit(-1)
-    })
-
+    this.pool = new Pool(CONFIG)
     this.connection()
   }
 
-  private async connection(attempt: number = 1) {
+  private async connection() {
     try {
       this.client = await this.pool.connect()
-      console.log('Connected to PostgreSQL!')
     } catch (error) {
-      console.error(`Connection attempt ${attempt} failed, error: ${error}`)
-      if (attempt < 5) {
-        const delay = 2 ** attempt * 1000 // Exponential backoff
-        console.log(
-          `Retrying connection attempt ${attempt + 1} in ${delay / 1000}s`,
-        )
-        await new Promise((resolve) => setTimeout(resolve, delay))
-        this.connection(attempt + 1)
-      } else {
-        throw new Error(`All connection attempts failed, error: ${error}`)
-      }
+      console.error(`erro na conexão, erro ${error}`)
+
+      throw new Error(`erro na conexão, erro ${error}`)
     }
   }
 
-  get clientInstance() {
+  get clientIstance() {
     return this.client
   }
 }

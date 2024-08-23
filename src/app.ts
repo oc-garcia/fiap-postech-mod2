72ff12795fa90b0c1678531702efa6ff0b4eb09a
oc-garcia <octaviogarcia1988@gmail.com>
2024-08-23 10:10:55 -0300

    Refactor database connection configuration and update Dockerfile, docker-compose.yml

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { postRoutes } from './http/controllers/post/routes'
 import { roleRoutes } from './http/controllers/role/routes'
 import { userRoleRoutes } from './http/controllers/userRole/routes'
 import { globalErrorHandler } from './utils/global-error-handler'
+import fastifyCors from '@fastify/cors'
 
 export const app = fastify()
 
@@ -14,6 +15,10 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   sign: { expiresIn: '10h' },
 })
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 //app.addHook('onRequest', validateJWT)
 
